37c47b090221c7d7a62b19087ebf676a87a245ef
mesge <james.gemmell@me.com>
2016-05-20 11:24:09 +1000

    Move configuration out of lib and into bin/console and spec helper

--- lib/wage_slave.rb
+++ lib/wage_slave.rb
@@ -10,6 +10,3 @@ require 'wage_slave/models/invoice'
 require 'wage_slave/payroll'
 require 'wage_slave/services/build_invoices'
 
-# Require config script
-Dotenv.load
-require 'wage_slave/demo_config_script'
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,7 +2,25 @@ $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'wage_slave'
 require 'wage_slave/models/invoice'
 require 'wage_slave/services/build_invoices'
-require 'wage_slave/demo_config_script'
+
+# Configure testable environment
+require 'dotenv'
+
+Dotenv.load
+
+# Configure Application with Demo API Keys
+WageSlave.configure do | config |
+	config.financial_institution = "ANZ"
+	config.bank_code 						 = "123-456"
+	config.user_id							 = "000001"
+	config.description					 = "No Yelling Commission"
+end
+
+WageSlave.configure_xero(
+  ENV['DEMO_XERO_CONSUMER_KEY'], 
+  ENV['DEMO_XERO_CONSUMER_SECRET'], 
+  File.expand_path("../../", __FILE__) + ENV['XERO_PEM_FILE_LOCATION']
+)
 
 require 'minitest'
 require 'minitest/spec'
