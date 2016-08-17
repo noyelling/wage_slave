c31fcc30243472810e2ad8b7cdf4493623f97d31
Jasper Boyschau <jasper@noyelling.com.au>
2016-08-17 14:40:16 +1000

    include config defaults in Batch method

--- lib/wage_slave/aba.rb
+++ lib/wage_slave/aba.rb
@@ -1,8 +1,14 @@
-
 module WageSlave
 	class Aba
 		def self.batch(attrs = {}, transactions = [])
-			Aba::Batch.new(attrs, transactions)
+			Aba::Batch.new({
+				financial_institution: attrs[:financial_institution] || WageSlave.configuration.financial_institution,
+	      bsb: attrs[:bsb] || WageSlave.configuration.bank_code,
+	      user_name: attrs[:user_name] || WageSlave.configuration.user_name,
+	      user_id: attrs[:user_id] || WageSlave.configuration.user_id,
+	      process_at: attrs[:process_at] || Time.now.strftime("%d%m%y"),
+	      description: attrs[:description] || "Payroll"
+			}, transactions)
 		end
 	end
 end
