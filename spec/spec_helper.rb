60beface97c4a2d176660758748bb1178fb64bd7
mesge <james.gemmell@me.com>
2016-08-29 13:52:46 +1000

    Add WageSlave setup to spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -17,3 +17,12 @@ require 'minitest/autorun'
 require 'minitest/reporters'
 
 Minitest::Reporters.use! [Minitest::Reporters::SpecReporter.new(:color => true)]
+
+WageSlave.configure do | config |
+	config.financial_institution                  = "ANZ" # Name of your bank
+	config.bank_code                              = "123-456" # i.e. BSB, Sort code etc
+	config.user_id                                = "123456" # i.e. CRN, Acc no. etc
+	config.description                            = "A default description"
+	config.user_name                              = "username"
+	config.xero = {}
+end
