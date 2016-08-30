b06bed279e3fac9c3a69b1c268d7cdbd3650827f
mesge <james.gemmell@me.com>
2016-08-30 17:39:04 +1000

    Fix indenting

--- lib/wage_slave/aba.rb
+++ lib/wage_slave/aba.rb
@@ -1,71 +1,71 @@
 module WageSlave
   class ABA
 
-		attr_reader :descriptive_record, :details
+    attr_reader :descriptive_record, :details
 
     def initialize(transactions = [])
       @descriptive_record = WageSlave::ABA::DescriptiveRecord.new
-			@details = WageSlave::ABA::DetailCollection.new(transactions)
+      @details = WageSlave::ABA::DetailCollection.new(transactions)
     end
 
-		def to_s
-			output = @descriptive_record.to_s + "\r\n"
-			output += @details.to_s + "\r\n"
+    def to_s
+      output = @descriptive_record.to_s + "\r\n"
+      output += @details.to_s + "\r\n"
 
-			# Record Type
-			# Size: 1
-			# Char position: 1
-			# Must be 7
-			output += "7"
+      # Record Type
+      # Size: 1
+      # Char position: 1
+      # Must be 7
+      output += "7"
 
-			# BSB format filler
-			# Size: 7
-			# Char position: 2-8
-			# Must be 999-999
-			output += "999-999"
+      # BSB format filler
+      # Size: 7
+      # Char position: 2-8
+      # Must be 999-999
+      output += "999-999"
 
-			# Reserved
-			# Size: 12
-			# Char position: 9-20
-			# Blank filled
-			output += " " * 12
+      # Reserved
+      # Size: 12
+      # Char position: 9-20
+      # Blank filled
+      output += " " * 12
 
-			# Net total amount
-			# Size: 10
-			# Char position: 21-30
-			# Right justified, zero filled.
-			output += @details.net_total.abs.to_s.rjust(10, "0")
+      # Net total amount
+      # Size: 10
+      # Char position: 21-30
+      # Right justified, zero filled.
+      output += @details.net_total.abs.to_s.rjust(10, "0")
 
-			# Credit total amount
-			# Size: 10
-			# Char position: 31-40
-			# Right justified, zero filled.
-			output += @details.credit_total.abs.to_s.rjust(10, "0")
+      # Credit total amount
+      # Size: 10
+      # Char position: 31-40
+      # Right justified, zero filled.
+      output += @details.credit_total.abs.to_s.rjust(10, "0")
 
-			# Debit total amount
-			# Size: 10
-			# Char position: 41-50
-			# Right justified, zero filled.
-			output += @details.debit_total.abs.to_s.rjust(10, "0")
+      # Debit total amount
+      # Size: 10
+      # Char position: 41-50
+      # Right justified, zero filled.
+      output += @details.debit_total.abs.to_s.rjust(10, "0")
 
-			# Reserved
-			# Size: 24
-			# Char position: 51-74
-			# Blank filled
-			output += " " * 24
+      # Reserved
+      # Size: 24
+      # Char position: 51-74
+      # Blank filled
+      output += " " * 24
 
-			# Count of Type 1 records
-			# Size: 6
-			# Char position: 75-80
-			# Right justified, zero filled.
-			output += @details.size.to_s.rjust(6, "0")
+      # Count of Type 1 records
+      # Size: 6
+      # Char position: 75-80
+      # Right justified, zero filled.
+      output += @details.size.to_s.rjust(6, "0")
 
-			# Reserved
-			# Size: 40
-			# Char position: 81-120
-			# Blank filled
-			output += " " * 40
-		end
+      # Reserved
+      # Size: 40
+      # Char position: 81-120
+      # Blank filled
+      output += " " * 40
+    end
 
   end
 end
--- lib/wage_slave/aba/detail_collection.rb
+++ lib/wage_slave/aba/detail_collection.rb
@@ -1,33 +1,33 @@
 module WageSlave
-	class ABA
-		class DetailCollection < Array
+  class ABA
+    class DetailCollection < Array
 
-			def initialize(details = [])
-				self.concat(
-					details.map { |d| WageSlave::ABA::DetailRecord.new(d) }
-				)
-			end
+      def initialize(details = [])
+        self.concat(
+          details.map { |d| WageSlave::ABA::DetailRecord.new(d) }
+        )
+      end
 
-			def net_total
-				self.credit_total + self.debit_total
-			end
+      def net_total
+        self.credit_total + self.debit_total
+      end
 
-			def credit_total
-				self
-					.select { |d| d.amount > 0 }
-					.reduce(0) { |acc, n| acc + n.amount }
-			end
+      def credit_total
+        self
+          .select { |d| d.amount > 0 }
+          .reduce(0) { |acc, n| acc + n.amount }
+      end
 
-			def debit_total
-				self
-					.select { |d| d.amount < 0 }
-					.reduce(0) { |acc, n| acc + n.amount }
-			end
+      def debit_total
+        self
+          .select { |d| d.amount < 0 }
+          .reduce(0) { |acc, n| acc + n.amount }
+      end
 
-			def to_s
-				self.map(&:to_s).join("\r\n")
-			end
+      def to_s
+        self.map(&:to_s).join("\r\n")
+      end
 
-		end
-	end
+    end
+  end
 end
